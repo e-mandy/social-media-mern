21c58ef237cf3639d30658511fcc0b88cbf423a7
e-mandy <andymfrd02@gmail.com>
2025-10-10 09:17:01 +0100

    On reprend tout seul

--- backend/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-  .connect(
-    "mongodb+srv://" + process.env.DB_USER_PASS + "@cluster0.j31roky.mongodb.net/social-media"
-  )
-  .then(console.log("Connection establish with the database"))
-  .catch((err) => console.log(err));
