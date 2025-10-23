fdb62f2c2b1727d84d1c21c202a371cd8da2e5cc
e-mandy <andymfrd02@gmail.com>
2025-10-23 15:00:30 +0100

    Mise en place du login (sans intégration du token jwt)

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import userModel from '../models/user.model.js'
+import { bcrypt } from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const register = async (req, res) => {
     const { pseudo, email, password } = req.body;
@@ -14,6 +16,22 @@ const register = async (req, res) => {
     }catch(err){
         res.status(401).json(err.message)
     }
+}
+
+const login = async (req, res)=>{
+    const { email, password } = req.body;
+
+    const targetUser = await userModel.findOne({ email: email })
+
+    if(!targetUser) return res.status('401').json({ message: "Email or password invalid" })
+
+    const passwordMatch = await bcrypt.compare(password, targetUser.password);
+
+    if(!passwordMatch) return res.status(401).json({ message: "Invalid password"})
+
+    const userToken = jwt.sign({ userId: targetUser._id }, process.env.APPLICATION_SECRET_KEY, { expiresIn: process.env.EXPIRING_DAY })
+
+    return res.status(200).cookie()
 }
 
 export { register }
