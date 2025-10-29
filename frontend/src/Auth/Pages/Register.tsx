251d85af9186008ac3103140a421d443eae08da1
e-mandy <andymfrd02@gmail.com>
2025-10-29 14:22:08 +0100

    Finalisation de la premiere partie UI de l'inscription

--- frontend/src/Auth/Pages/Register.tsx
+++ frontend/src/Auth/Pages/Register.tsx
@@ -4,31 +4,34 @@ import { Mail, User, Lock } from 'lucide-react'
 const Register = () => {
   return (
     <div>
-        <div className="bg-auth-primary h-screen w-screen">
-          <div className="flex flex-col items-center">
-            <h1>Welcome in the experience</h1>
-            <p>Already have an account ?<a href="">Sign in</a></p>
-            <form>
+        <div className="bg-auth-primary h-screen w-screen flex">
+          <div className="flex flex-col items-center my-auto w-full">
+            <h1 className="text-2xl font-semibold">Welcome in the experience</h1>
+            <p className="text-secondary mt-1 mb-6">Already have an account ? <a className="text-blue-400" href="">Sign in</a></p>
+            <form className="w-[85%]">
+
               <div>
-                <label htmlFor="pseudo">
-                    <User />
-                  <input type="text" placeholder="Your pseudo" id="pseudo" />
-                </label>
-              </div>
-              <div>
-                <label htmlFor="email">
-                    <Mail />
-                  <input type="email" placeholder="Email adress" id="email" />
-                </label>
-              </div>
-              <div>
-                <label htmlFor="password">
-                  <Lock />
-                  <input type="password" id="password" placeholder="Your secret password" />
+                <label className="input-class bg-black" htmlFor="pseudo">
+                  <User color="gray" />
+                  <input type="text" className="outline-none" placeholder="Your pseudo" id="pseudo" />
                 </label>
               </div>
 
-              <button type="submit">S'inscrire</button>
+              <div>
+                <label className="input-class bg-black" htmlFor="email">
+                  <Mail color="gray" />
+                  <input type="email" className="outline-none" placeholder="Email Address" id="email" />
+                </label>
+              </div>
+
+              <div>
+                <label className="input-class bg-black" htmlFor="pseudo">
+                  <Lock color="gray" />
+                  <input type="password" className="outline-none" placeholder="Secret Password" id="pseudo" />
+                </label>
+              </div>
+
+              <button className="button-class bg-blue-600 w-full mt-2" type="submit">S'inscrire</button>
             </form>
 
           </div>
